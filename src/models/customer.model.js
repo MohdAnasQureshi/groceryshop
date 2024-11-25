4fd2c3658dee365c086ee0a8c5bb8a9300de96e9
MohdAnasQureshi <qureshianas471@gmail.com>
2024-11-26 00:09:40 +0530

    add user and customer model

---
+++ src/models/customer.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const customerSchema = new Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    customerOf: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    customerContact: {
+      type: String,
+      required: true,
+    },
+    totalOutstandingDebt: {
+      type: Number,
+      default: 0,
+    },
+    totalPaid: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+customerSchema.plugin(mongooseAggregatePaginate);
+
+export const Customer = mongoose.model("Customer", customerSchema);
